89388f27cb63fc815f2d0268ab5b3566a831bd66
Beaumann Marc <beaumann.marc@laposte.net>
2017-01-20 21:23:46 +0100

    un cube qui bouge

---
+++ GGJ/Assets/Scripts/Mob_horizontal_patrol.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mob_horizontal_patrol : MonoBehaviour {
+
+	public Vector3 initPos;
+	public float speed = 2.0f;
+	public bool right = true;
+	public float amplitude = 4.0f;
+	public bool token = true;
+	// Use this for initialization
+	void Start () {
+		initPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(token) {
+			transform.Translate(Vector2.up * speed * Time.deltaTime);
+			if(right) {
+				if(transform.position.x - initPos.x >= amplitude) {
+					token = false;
+					right = !right;
+					StartCoroutine("rotate");
+				}
+			}
+			else {
+				if(transform.position.x - initPos.x <= -amplitude) {
+					token = false;
+					right = !right;
+					StartCoroutine("rotate");
+				}
+			}
+		}
+
+	}
+
+	public IEnumerator rotate() {
+		yield return new WaitForSeconds(1);
+		transform.Rotate(0, 0, 90);
+		yield return new WaitForSeconds(1);
+		transform.Rotate(0, 0, 90);
+		token = true;
+
+	}
+
+}
---
+++ GGJ/Assets/Scripts/Player_move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_move : MonoBehaviour {
+
+	public Transform trans;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		trans = this.GetComponent<Transform>();
+		speed = 150.0f;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		transform.Translate(x, y, 0);
+	}
+}
