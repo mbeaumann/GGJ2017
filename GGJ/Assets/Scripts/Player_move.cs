5e6d84ed4d9242d05dbef350624a121048d73b45
Beaumann Marc <beaumann.marc@laposte.net>
2017-01-21 14:00:49 +0100

    leap

--- GGJ/Assets/Scripts/Player_move.cs
+++ GGJ/Assets/Scripts/Player_move.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class Player_move : MonoBehaviour {
 
 	public Transform trans;
-	public float speed = 90.0f;
+	public float speed = 10.0f;
+	public float leap_speed = 30.0f;
+	public int endurance = 100;
+	public bool can_leap = true;
+	public int endu_decay = 5;
+	public int endu_regen = 1;
 	// Use this for initialization
 	void Start () {
 		trans = this.GetComponent<Transform>();
@@ -13,8 +18,26 @@ public class Player_move : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-		var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		float x;
+		float y;
+		if(endurance == 100) {
+			can_leap = true;
+		}
+		if(Input.GetKey(KeyCode.LeftShift) && can_leap) {
+			endurance -= endu_decay;
+			if(endurance <= endu_decay) {
+				can_leap = false;
+			}
+			x = Input.GetAxis("Horizontal") * Time.deltaTime * leap_speed;
+			y = Input.GetAxis("Vertical") * Time.deltaTime * leap_speed;
+		}
+		else {
+			if(endurance < 100) {
+				endurance += endu_regen;
+			}
+			x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+			y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		}
 
 		transform.Translate(x, 0, y);
 	}
