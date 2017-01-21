3d161671087e3f4d5accaf9c9b381170e9ac5774
Beaumann Marc <beaumann.marc@laposte.net>
2017-01-21 14:19:18 +0100

    le pc de julien il est tout nul :(

--- GGJ/Assets/Scripts/Torch.cs
+++ GGJ/Assets/Scripts/Torch.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using UnityEngine.UI;
+using UnityEditor.SceneManagement;
 
 public class Torch : MonoBehaviour {
+	
+	public int nbRays = 5;
+	public int angle = 25;
+	public int portee = 10;
 
-	public void OnTriggerEnter2D(Collider2D col){
+	void Update(){
+		castRays ();
+	}
+
+
+	void castRays(){
+
+		int gap = angle / nbRays;
+		int angleMin = (int)(-angle * 0.5f);
+		int angleMax = (int)(angle * 0.5f);
+			
+		RaycastHit hit;
+		for (int i = angleMin; i < angleMax; i += gap) {
+		
+			Vector3 vec = (Quaternion.Euler (0, i, 0) * transform.forward).normalized * portee;
+			if (Physics.Raycast(transform.position, vec, out hit, portee)) {
+				if (hit.collider.gameObject.tag == "Player") {
+					hit.collider.gameObject.SendMessage ("kill");
+				}
+			}
+			Debug.DrawRay (transform.position, vec, Color.red);
+		}
+	}
+
+	public void OnTriggerEnter(Collider col){
 		if (col.gameObject.tag == "Player") {
 			col.gameObject.SendMessage ("kill");
 		}
